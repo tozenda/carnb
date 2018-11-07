57354959d74f568618ef0b961eab5b4e2d9fd5f5
JoffreyFerreira <mehrunesdagon26@gmail.com>
2018-11-07 15:35:05 +0100

    squellette du provider

--- carnb/src/providers/list-voiture/list-voiture.ts
+++ carnb/src/providers/list-voiture/list-voiture.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { UrlSerializer } from 'ionic-angular';
 import { User } from '../../entities/user';
+import { Voiture } from '../../entities/voiture';
+import { Reservation } from '../../entities/reservation';
 
 /*
   Generated class for the ListVoitureProvider provider.
@@ -13,8 +15,36 @@ export class ListVoitureProvider {
   CurrentUser: User;
   Location: number[];
 
+  voitureList: Voiture[];
+  userList: User[];
+  reservationList:Reservation[];
+
+
   constructor() {
-    console.log('Hello ListVoitureProvider Provider');
+    
   }
+
+  addVoiture(voiture: Voiture, user:User){
+  	this.voitureList.push(voiture);
+
+  }
+
+  addUser(user: User){
+  	this.userList.push(user);
+  }
+
+  addReservation(){
+
+  }
+
+  getVoiture(user:User){
+  	let voitureList: Voiture[] = user.getVoitureList();
+  	return voitureList;
+  }
+
+  getUser(nom:string, prenom:string){
+
+  }
+
 
 }
